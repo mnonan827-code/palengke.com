3247db65f439679b302bc6b3ddc7a13e29af287b
Francis Nonan <mnonan827@gmail.com>
2025-11-02 10:24:12 +0800

    Fix modal scrolling - buttons always visible

--- app.js
+++ app.js
@@ -882,8 +882,8 @@ window.showModal = function(titleHtml, contentHtml, actionsHtml = '') {
               </div>
               <div><button onclick="hideModal()" class="text-gray-500 hover:text-gray-700"><i data-lucide="x" class="w-5 h-5"></i></button></div>
           </div>
-          <div class="mt-4 text-gray-700">${contentHtml}</div>
         </div>
+        <div class="text-gray-700">${contentHtml}</div>
         <div class="bg-gray-50 p-4 flex justify-end gap-3">
           ${actionsHtml}
         </div>
